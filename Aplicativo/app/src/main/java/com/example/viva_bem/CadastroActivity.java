7ac0e9bdfc1c616a471225bfb1c671570c6a268b
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-06-15 21:27:38 -0300

    Login e Cadastro Interface Básica

--- Aplicativo/app/src/main/java/com/example/viva_bem/CadastroActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/CadastroActivity.java
@@ -1,4 +1,20 @@
 package com.example.viva_bem;
 
-public class CadastroActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void navigateMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- Aplicativo/app/src/main/java/com/example/viva_bem/LoginActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.viva_bem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -8,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void navigateMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
