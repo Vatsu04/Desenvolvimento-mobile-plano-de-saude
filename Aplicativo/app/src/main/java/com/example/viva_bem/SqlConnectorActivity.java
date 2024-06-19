adb198e4222703ede41fba770665d1b072a7e20a
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-06-19 12:53:48 -0300

    Sql

---
+++ Aplicativo/app/src/main/java/com/example/viva_bem/SqlConnectorActivity.java
@@ -0,0 +1,61 @@
+package com.example.viva_bem;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SqlConnectorActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void fetchData(View view) {
+        new FetchDataTask().execute("http://localhost/api.php");
+    }
+
+    private class FetchDataTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0];
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    return sb.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                textView.setText(result);
+            } else {
+                textView.setText("Failed to fetch data");
+            }
+        }
+    }
+}
