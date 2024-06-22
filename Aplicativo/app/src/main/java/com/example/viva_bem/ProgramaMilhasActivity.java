509373ecfa61526d84879d8ff78519bb23375ad2
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-06-22 00:39:18 -0300

    Update ProgramaMilhasActivity.java

--- Aplicativo/app/src/main/java/com/example/viva_bem/ProgramaMilhasActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/ProgramaMilhasActivity.java
@@ -3,12 +3,27 @@ package com.example.viva_bem;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class ProgramaMilhasActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programa_milhas);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        findViewById(R.id.imageView6).setOnClickListener((view) -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
     }
 }
