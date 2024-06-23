2130b1402ce5231a02ef6ec05a94a764f2d8798c
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-06-23 17:40:04 -0300

    Update FeedbackActivity.java

--- Aplicativo/app/src/main/java/com/example/viva_bem/FeedbackActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/FeedbackActivity.java
@@ -1,81 +1,54 @@
-package com.example.viva_bem;
-
+package com.example.viva_bem;// Import statements
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FeedbackActivity extends AppCompatActivity {
-
-    private int milhas;
-    private TextView milhasTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        milhasTextView = findViewById(R.id.textView21);
-
-        // Retrieve milhas from intent if available
-        Intent intent = getIntent();
-        if (intent.hasExtra("milhas")) {
-            milhas = intent.getIntExtra("milhas", 0);
-        }
-
-        updateMilhasUI();
-
         Button escreverFeedbackButton = findViewById(R.id.feedbackCepem);
         escreverFeedbackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToEscreverFeedbackActivity();
+                navigateToEscreverFeedbackActivity("Clínica CEPEM");
             }
         });
 
-        Button escreverFeedbackButton2 = findViewById(R.id.buttonEscreverFeedback);
-        escreverFeedbackButton.setOnClickListener(new View.OnClickListener() {
+        Button escreverFeedbackButton2 = findViewById(R.id.feedbackLabi);
+        escreverFeedbackButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToEscreverFeedbackActivity();
+                navigateToEscreverFeedbackActivity("Laboratórios Labi");
             }
         });
 
-        Button escreverFeedbackButton3 = findViewById(R.id.buttonEscreverFeedback);
-        escreverFeedbackButton.setOnClickListener(new View.OnClickListener() {
+        Button escreverFeedbackButton3 = findViewById(R.id.feedbackFelippe);
+        escreverFeedbackButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToEscreverFeedbackActivity();
+                navigateToEscreverFeedbackActivity("Clínica Felippe Mattoso");
             }
         });
 
-        Button escreverFeedbackButton4 = findViewById(R.id.buttonEscreverFeedback);
-        escreverFeedbackButton.setOnClickListener(new View.OnClickListener() {
+        Button escreverFeedbackButton4 = findViewById(R.id.feedbackLamina);
+        escreverFeedbackButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navigateToEscreverFeedbackActivity();
+                navigateToEscreverFeedbackActivity("Lamina");
             }
         });
     }
 
-
-
-    private void updateMilhasUI() {
-        milhasTextView.setText(String.valueOf(milhas));
-    }
-
-    public void navigateToEscreverFeedbackActivity() {
+    private void navigateToEscreverFeedbackActivity(String clinicName) {
         Intent intent = new Intent(this, EscreverFeedbackActivity.class);
-        intent.putExtra("milhas", milhas);
-        startActivity(intent);
-    }
-
-    public void navigateBack() {
-        Intent intent = new Intent(this, MenuActivity.class);
-        intent.putExtra("milhas", milhas);
+        intent.putExtra("CLINIC_NAME", clinicName);
         startActivity(intent);
     }
 }
