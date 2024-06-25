02b7f39c5506cd1a9d5467202989fa976b1d2667
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-06-24 23:26:09 -0300

    Update

--- Aplicativo/app/src/main/java/com/example/viva_bem/AgendarConsultaActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/AgendarConsultaActivity.java
@@ -28,6 +28,31 @@ public class AgendarConsultaActivity extends AppCompatActivity {
         milhasTextView.setText(String.valueOf(milhas));
     }
 
+    public void navigateAgendamentoCepem() {
+        Intent intent = new Intent(this, AgendamentosActivity.class);
+        intent.putExtra("CLINIC_NAME", "CEPEM");
+        startActivity(intent);
+    }
+
+    public void navigateAgendamentoLabi() {
+        Intent intent = new Intent(this, AgendamentosActivity.class);
+        intent.putExtra("CLINIC_NAME", "Laboratórios Labi");
+        startActivity(intent);
+    }
+
+    public void navigateAgendamentoFelippe() {
+        Intent intent = new Intent(this, AgendamentosActivity.class);
+        intent.putExtra("CLINIC_NAME", "Clínica Felippe Mattoso");
+        startActivity(intent);
+    }
+
+    public void navigateAgendamentoLamina() {
+        Intent intent = new Intent(this, AgendamentosActivity.class);
+        intent.putExtra("CLINIC_NAME", "Clínica Lâmina");
+        startActivity(intent);
+    }
+
+
     public void navigateBack() {
         Intent intent = new Intent(this, MenuActivity.class);
         intent.putExtra("milhas", milhas);
--- Aplicativo/app/src/main/java/com/example/viva_bem/ContatoActivity.java
+++ Aplicativo/app/src/main/java/com/example/viva_bem/ContatoActivity.java
@@ -8,22 +8,23 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class SobreActivity extends AppCompatActivity {
+public class ContatoActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sobre);
+        setContentView(R.layout.acticity_contato);
         drawerLayout = findViewById(R.id.drawer_layout);
 
-        findViewById(R.id.imageView14).setOnClickListener((view) -> {
+        findViewById(R.id.imageView22).setOnClickListener((view) -> {
             drawerLayout.openDrawer(GravityCompat.START);
         });
 
-        NavigationView navigationView = findViewById(R.id.navigationView);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setItemIconTintList(null);
 
     }
+
 }
